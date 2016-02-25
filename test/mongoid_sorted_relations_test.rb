7aa5a56c0d5b92a0b8ff1270a90f1c8115fc38f1
Tomas Celizna <tomas.celizna@gmail.com>
2016-02-25 11:09:37 +0100

    failing test

--- test/mongoid_sorted_relations_test.rb
+++ test/mongoid_sorted_relations_test.rb
@@ -5,15 +5,19 @@ describe MongoidSortedRelations do
   let(:b_document_2) { BDocument.new }
   let(:b_document_3) { BDocument.new }
 
-  let(:a_document) { ADocument.create }
+  let(:a_document) { ADocument.new }
 
   before do
-    a_document.b_documents << b_document_1
-    a_document.b_documents << b_document_2
-    a_document.b_documents << b_document_3
+    a_document.save!
+    b_document_1.save!; b_document_2.save!; b_document_3.save!
+
+    a_document.b_document_ids = [b_document_2.id, b_document_3.id, b_document_1.id]
+    a_document.save
+    
+    a_document.reload
   end
 
   it 'preserves the give order' do
-    a_document.b_documents.must_equal [b_document_1, b_document_2, b_document_3]
+    a_document.b_documents.must_equal [b_document_2, b_document_3, b_document_1]
   end
 end
